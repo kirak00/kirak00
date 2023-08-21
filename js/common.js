f9b55a554d7b688111c4f2e4aca992d9bf372020
rubjin <jjh@iwellcomm.com>
2023-08-21 14:48:18 +0900

    faq

--- js/common.js
+++ js/common.js
@@ -16,7 +16,7 @@ $(function(){
   /* Range 달력 */
   datepickerRange_init();
 
-
+  accordion();
   tabScript()
 
 });
@@ -311,4 +311,42 @@ function faq(){
   })
 }
 
- 
+function accordion() {
+  class Accordion {
+    constructor(domNode) {
+      this.rootEl = domNode;
+      this.buttonEl = this.rootEl.querySelector('button[aria-expanded]');
+      const controlsId = this.buttonEl.getAttribute('aria-controls');
+      this.contentEl = document.getElementById(controlsId);
+      this.open = this.buttonEl.getAttribute('aria-expanded') === 'true';
+      this.buttonEl.addEventListener('click', this.onButtonClick.bind(this));
+    }
+    onButtonClick() {
+      this.toggle(!this.open);
+    }
+    toggle(open) {
+      if (open === this.open) {
+        return;
+      }
+      this.open = open;
+      this.buttonEl.setAttribute('aria-expanded', `${open}`);
+      if (open) {
+        this.contentEl.removeAttribute('hidden');
+      } else {
+        this.contentEl.setAttribute('hidden', '');
+      }
+    }
+    open() {
+      this.toggle(true);
+    }
+    close() {
+      this.toggle(false);
+    }
+  }
+
+  const accordions = document.querySelectorAll('.accordion h3');
+  accordions.forEach((accordionEl) => {
+    new Accordion(accordionEl);
+  });
+
+} 
