4f856f10bbe4fe537ac797c09039379d77215f75
rubjin <jjh@iwellcomm.com>
2023-09-11 17:57:21 +0900

    작업중간커밋

--- js/common.js
+++ js/common.js
@@ -546,29 +546,39 @@ function accordion() {
   class Accordion {
     constructor(domNode) {
       this.rootEl = domNode;
-      this.buttonEl = this.rootEl.querySelector('button[aria-expanded]');
+      this.buttonEl = this.rootEl.querySelector('[aria-expanded]');
       const controlsId = this.buttonEl.getAttribute('aria-controls');
       this.contentEl = document.getElementById(controlsId);
       this.open = this.buttonEl.getAttribute('aria-expanded') === 'true';
       this.buttonEl.addEventListener('click', this.onButtonClick.bind(this));
     }
-    onButtonClick() {
+    onButtonClick(e) {
       const accordionOption = this.buttonEl.closest('[accordion-option]').getAttribute('accordion-option');
+      var $exceptTarget = this.buttonEl.querySelector(" button, .checxkbox, label, input[type=checkbox]");
+     
       if (accordionOption === 'only') { //only type
         if (this.buttonEl.ariaExpanded === 'false') { //close 
           document.querySelectorAll('.accordionTrigger').forEach((trigger) => {
+            if (!($(e.target).is($exceptTarget))) {
+              return;
+            }
             trigger.setAttribute('aria-expanded', 'false');
             this.buttonEl.setAttribute('aria-expanded', 'true');
-            this.buttonEl.closest('.accordionItem').classList.add('isOpen')
+            // this.buttonEl.closest('.accordionItem').classList.add('isOpen')
+            console.log('close')
+            
           })
           document.querySelectorAll('.accordionPanel').forEach((panel) => {
             panel.setAttribute('hidden', '');
             this.contentEl.removeAttribute('hidden');
+            console.log('open')
           })
+          return;
         } else { // open
           this.buttonEl.setAttribute('aria-expanded', 'false');
           this.contentEl.setAttribute('hidden', '');
-          this.buttonEl.closest('.accordionItem').classList.remove('isOpen')
+          // this.buttonEl.closest('.accordionItem').classList.remove('isOpen')
+          return;
         }
       } else { //toggle type
         this.toggle(!this.open);
@@ -628,6 +638,7 @@ $.fn.calcTextareaHeight = function(e) {
     });
     if (_this.val().length > 0) {
       let scrollHeight = _this.prop('scrollHeight');
+      console.log('scrollHeight',scrollHeight)
       _this.css("height", scrollHeight+4+"px");
     } 
    
