2690b9656e65fef8c6cd8d78c7e725de6d32e48a
kirak00 <krdeem@gmail.com>
2023-10-05 14:40:02 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -2,24 +2,29 @@
 /*  jquery.contrast-finder.js */
 var colorType="";function isValidateColorName(c){var d=["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen","RebeccaPurple"];var b=c.trim().toLowerCase();var a="";d.some(function(f,e){if(b===f.toLowerCase()){a=f;return true}});if(a!==""){return a}return false}function isValidateColorHex(a){a=a.trim();if(a.match(/^#?[a-f0-9]{6}$/i)!==null||a.match(/^#?[a-f0-9]{3}$/i)!==null){if(a.substr(0,1)!=="#"){a="#"+a}return a}return false}function isValideRgbMaxValue(e){var a=e.match(/(\d{1,3})/g);if(a!==null){var d=true;for(var b=0,f=a.length;b<f;b++){if(a[b]>255){d=false}}return d}return false}function isValidateColorRgb(a){a=a.trim();if(a.match(/^rgb\((\d{1,3}),(\d{1,3}),(\d{1,3})\)?$/i)!==null){if(isValideRgbMaxValue(a)===true){if(a.substr(-1)!==")"){a=a+")"}return a}}else{if(a.match(/^(\d{1,3}),(\d{1,3}),(\d{1,3})$/i)!==null){if(isValideRgbMaxValue(a)===true){return"rgb("+a+")"}}}return false}function isValidateColor(c){c=c.trim();var b=c.match(/,/g);var a=false;if(b!==null){if(b.length===2){a=isValidateColorRgb(c);if(a!==false){colorType="rgb"}}}else{a=isValidateColorHex(c);if(a!==false){colorType="hex"}else{a=isValidateColorName(c);if(a!==false){colorType="name"}}}return a}function hexToRgb(c){if(c.match(/^#?[a-f0-9]{3}$/i)!==null){var b=/^#?([a-f\d]{1})([a-f\d]{1})([a-f\d]{1})$/i.exec(c);c=b[1]+b[1]+b[2]+b[2]+b[3]+b[3]}var a=/^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(c);return"rgb("+parseInt(a[1],16)+","+parseInt(a[2],16)+","+parseInt(a[3],16)+")"}function rgbToHex(c){var b=c.replace(/[^\d,]/g,"").split(",");return"#"+((1<<24)+(+b[0]<<16)+(+b[1]<<8)+ +b[2]).toString(16).slice(1)}function computeContrast(a,b){return Math.round((a+0.05)/(b+0.05)*100)/100}function getComposantValue(a){var b=a/255;if(b<=0.03928){return b/12.92}else{return Math.pow(((b+0.055)/1.055),2.4)}}function getLuminosity(b){var f=isValidateColor(b.toString());if(colorType==="hex"){b=hexToRgb(b)}var e=/(.*?)rgb\((\d+),(\d+),(\d+)\)/.exec(b);var f=parseInt(e[2]);var d=parseInt(e[3]);var a=parseInt(e[4]);var c=getComposantValue(f)*0.2126+getComposantValue(d)*0.7152+getComposantValue(a)*0.0722;return c}function getContrastRatio(d,c){var a=getLuminosity(d);var b=getLuminosity(c);if(a>b){return computeContrast(a,b)}else{return computeContrast(b,a)}}$.extend($.krcode,{contrast:{version:"0.1.0"}});$.fn.contrast=function(options){var This=$(this);This.css("color");var a=This.css("color").replace(/ /g,"");var b=This.css("background-color").replace(/ /g,"");console.log(getContrastRatio(a,b));return getContrastRatio(a,b)};$.contrastAll=function(options){$("*").each(function(){var This=$(this);This.css("color");var a=This.css("color").replace(/ /g,"");var b=This.css("background-color").replace(/ /g,"");if(b.indexOf("rgba")>=0)return true;var d=getContrastRatio(a,b);if(d<3.5){console.log(d,this)}})};
 
-$.datepicker.setDefaults({
-  dateFormat: 'yymmdd',
-  prevText: '이전 달',
-  nextText: '다음 달',
-  monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-  monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-  dayNames: ['일', '월', '화', '수', '목', '금', '토'],
-  dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
-  dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
-  showMonthAfterYear: true,
-  yearSuffix: '년',
-  buttonImage: "../../images/common/ico_24_calendar_primary.png",
-  buttonText: "달력입력",
-  buttonImageOnly: false,
-  showOn: "button",
-dateFormat: "yy-mm-dd"
-});
+try {
+  $.datepicker.setDefaults({
+    dateFormat: 'yymmdd',
+    prevText: '이전 달',
+    nextText: '다음 달',
+    monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+    dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+    dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+    showMonthAfterYear: true,
+    yearSuffix: '년',
+    buttonImage: "../../images/common/ico_24_calendar_primary.png",
+    buttonText: "달력입력",
+    buttonImageOnly: false,
+    showOn: "button",
+  dateFormat: "yy-mm-dd"
+  });
 
+  
+} catch (error) {
+  
+}
 
 
 
@@ -58,10 +63,14 @@ $(function(){
 
 
   
-
+  try {
+    
   $( ".calendar" ).datepicker({
   });
 
+  } catch (error) {
+    
+  }
 
   /* 접근성 체크 운영 삭제  */
   /* 접근성 체크 운영 삭제  */
@@ -316,6 +325,12 @@ function gnbAction(){
   })
   $(".gnb a").on("blur",function(){
     gnbOff();
+  })
+
+  $(".gnb li").hover(function(){
+    $(this).addClass("hover")
+  },function(){
+    $(this).removeClass("hover")
   })
 
   $(".headerCont").hover(function(){
@@ -613,30 +628,31 @@ function accordion() {
       const controlsId = this.buttonEl.getAttribute('aria-controls');
       this.contentEl = document.getElementById(controlsId);
       this.open = this.buttonEl.getAttribute('aria-expanded') === 'true';
-      this.buttonEl.addEventListener('click', this.onButtonClick.bind(this));
+      this.buttonEl.evt = this.onButtonClick;
+      this.buttonEl.This = this;
     }
-    onButtonClick(e) {
-      const accordionOption = this.buttonEl.closest('[accordion-option]').getAttribute('accordion-option');
+    onButtonClick(e , This) {
+      const accordionOption = This.buttonEl.closest('[accordion-option]').getAttribute('accordion-option');
       if (accordionOption === 'only') { //only type
-        if (this.buttonEl.ariaExpanded === 'false') { //close 
-          this.buttonEl.closest('[accordion-option]').querySelectorAll('.accordionTrigger').forEach((trigger) => {
+        if (This.buttonEl.ariaExpanded === 'false') { //close 
+          This.buttonEl.closest('[accordion-option]').querySelectorAll('.accordionTrigger').forEach((trigger) => {
             trigger.setAttribute('aria-expanded', 'false');
-            this.buttonEl.setAttribute('aria-expanded', 'true');
+            This.buttonEl.setAttribute('aria-expanded', 'true');
           })
-          this.buttonEl.closest('[accordion-option]').querySelectorAll('.accordionPanel').forEach((panel) => {
+          This.buttonEl.closest('[accordion-option]').querySelectorAll('.accordionPanel').forEach((panel) => {
             panel.closest('.accordionItem')?.classList.remove("isOpen")
             panel.setAttribute('hidden', '');
-            this.contentEl.removeAttribute('hidden');
+            This.contentEl.removeAttribute('hidden');
           });
-          this.buttonEl.parentNode.parentNode.classList.add("isOpen")
+          This.buttonEl.parentNode.parentNode.classList.add("isOpen")
         } 
         else { // open
-          this.buttonEl.parentNode.parentNode.classList.remove("isOpen")
-          this.buttonEl.setAttribute('aria-expanded', 'false');
-          this.contentEl.setAttribute('hidden', '');
+          This.buttonEl.parentNode.parentNode.classList.remove("isOpen")
+          This.buttonEl.setAttribute('aria-expanded', 'false');
+          This.contentEl.setAttribute('hidden', '');
         }
       } else { //toggle type
-        this.toggle(!this.open);
+        This.toggle(!This.open);
       }
       // if($(".textarea.autoHeight").length >0) $('.textarea.autoHeight').calcTextareaHeight();
     }
@@ -661,9 +677,17 @@ function accordion() {
   }
 
   const accordions = document.querySelectorAll('.accordion .accordionTitle');
-  accordions.forEach((accordionEl) => {
-    new Accordion(accordionEl);
+  // accordions.forEach((accordionEl) => {
+  //   new Accordion(accordionEl);
+  // });
+  
+  $(document).on('click', '.accordion .accordionTitle button',  function(e){
+    if(!e.target.This){
+      new Accordion(e.target.parentNode);
+    }
+    e.target.evt(e, e.target.This)
   });
+  
 
 } 
 
